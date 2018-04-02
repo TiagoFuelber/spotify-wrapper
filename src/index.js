fa5c779ae605c49a23e133844e0dfca938cc8217
Tiago Fuelber <tfuelber@gmail.com>
2018-04-01 23:54:14 -0300

    getAlbum,

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import {
+  search,
+  searchArtist,
+  searchAlbums,
+  searchPlaylists,
+} from './search';
+
+import {
+  getAlbum,
+  getAlbums,
+  getAlbumTracks,
+} from './album';
+
+module.exports = {
+  search,
+  searchArtist,
+  searchAlbums,
+  searchPlaylists,
+  getAlbum,
+  getAlbums,
+  getAlbumTracks,
+};
--- tests/search.spec.js
+++ tests/search.spec.js
@@ -9,7 +9,7 @@ sinonStubPromise(sinon);
 
 global.fetch = require('node-fetch');
 
-describe('Spotify Wrapper', () => {
+describe('Search', () => {
   let fetchedStub;
   let promise;
 
